993781fd9ce8dd80dbaf06ee5ba024595ac9d560
Cynthia7979 <CynthiaWang7979@hotmail.com>
2021-12-12 22:11:03 +0800

    figured out how to use flask-wtf

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # admin-panels
 * [问卷星](https://www.wjx.cn/wjx/design/designstart.aspx?activity=142844741) | [主页](https://www.wjx.cn/)
 * [Coggle](https://coggle.it/diagram/YbXYj7gXoT6Hq6K4/t/my-arg-maybe-clue-logic)
+* [Azure Portal](https://portal.azure.com/)
--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = open('key').read()
+
+Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    user_name = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
 
 
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    form = LoginForm()
+    return render_template('index.html', form=form)
