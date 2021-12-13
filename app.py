5f8a6b456a3462d9813e8fecfc1d12fe0143f50f
Cynthia7979 <CynthiaWang7979@hotmail.com>
2021-12-13 16:34:34 +0800

    Added admin_home and database pages

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ Bootstrap(app)
 
 
 class LoginForm(FlaskForm):
-    user_name = StringField('User Name', validators=[DataRequired()])
+    user_name = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
@@ -25,16 +25,41 @@ class LoginForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     form = LoginForm()
-    if form.validate_on_submit():
+    msg = ''
+    if form.is_submitted() and form.validate_on_submit():
         name, password = form.user_name.data, form.password.data
         if name == 'GhWor103' and password == 'GmDv%9dKY]':
-            return redirect(url_for('admin', passphrase=passphrase_of_today))
-    return render_template('index.html', form=form)
+            return redirect(url_for('admin_home', passphrase=passphrase_of_today))
+        elif name and password:
+            msg = 'Invalid Username or Password'
+        else:
+            msg = ''
+    return render_template('index.html', form=form, msg=msg)
 
 
-@app.route('/admin/?psp=<passphrase>')
-def admin(passphrase):
+@app.route('/admin_home/?<passphrase>')
+@app.route('/admin_home')
+def admin_home(passphrase=''):
     if passphrase != passphrase_of_today:
         abort(401)
     else:
-        return '<p>TODO(</p>'
+        return render_template('admin_home.html')
+
+
+@app.route('/database/?<passphrase>')
+@app.route('/database')
+def database(passphrase=''):
+    if passphrase != passphrase_of_today:
+        abort(401)
+    else:
+        return render_template('database.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(401)
+def invalid_credential(e):
+    return render_template('401.html'), 401
